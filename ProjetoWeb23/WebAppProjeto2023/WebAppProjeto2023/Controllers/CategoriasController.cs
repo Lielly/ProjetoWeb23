2258357ad65143273a7362deea3c0f9e4f4feead
Lielly Nátally <lielly.natally@academico.ifrn.edu.br>
2023-08-01 11:05:17 -0300

    Lista05 - cap 05

---
+++ ProjetoWeb23/WebAppProjeto2023/WebAppProjeto2023/Controllers/CategoriasController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Modelo.Tabelas;
+using WebAppProjeto2023.Models;
+
+namespace WebAppProjeto2023.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(
+                context.Categorias.OrderBy(context => context.Nome)
+            );
+        }
+
+        // GET: Categorias
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            context.Categorias.Add(categoria);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Edit/
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Edit/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        // GET: Details
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // GET: Delete
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Categoria categoria = context.Categorias.Find(id);
+            context.Categorias.Remove(categoria);
+            context.SaveChanges();
+            TempData["Message"] = "Categoria \"" + categoria.Nome + "\" foi removida";
+            return RedirectToAction("Index");
+        }
+    }
+}
